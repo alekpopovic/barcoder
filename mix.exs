6910e08f83e4f4893558bf7039334e08a0057e32
aleksandar-popovic <aleksandar.popovic@hotmail.com>
2025-06-14 18:40:58 +0200

    publish

--- mix.exs
+++ mix.exs
@@ -6,23 +6,34 @@ defmodule Barcoder.MixProject do
       app: :barcoder,
       version: "0.1.0",
       elixir: "~> 1.18",
-      start_permanent: Mix.env() == :prod,
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description() do
+    "Production-Ready Code 39 Barcode Generator"
+  end
+
+  defp package() do
+    [
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/alekpopovic/barcoder"}
     ]
   end
 end
